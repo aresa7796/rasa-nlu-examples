afaecdb42f05cb9893612040094515b5e27eb336
zhuzc <aresa@qq.com>
2021-11-16 10:20:08 +0800

    add bert_classifier

--- rasa_nlu_examples/classifiers/__init__.py
+++ rasa_nlu_examples/classifiers/__init__.py
@@ -1,4 +1,5 @@
 from .sparse_naive_bayes_intent_classifier import SparseNaiveBayesIntentClassifier
+from .bert_classifier import BertClassifier
 from .sparse_logistic_regression_intent_classifier import (
     SparseLogisticRegressionIntentClassifier,
 )
@@ -6,4 +7,5 @@ from .sparse_logistic_regression_intent_classifier import (
 __all__ = [
     "SparseNaiveBayesIntentClassifier",
     "SparseLogisticRegressionIntentClassifier",
+    "BertClassifier",
 ]
